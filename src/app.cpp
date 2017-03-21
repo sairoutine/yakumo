24dfc9bfeb0f74144832f443db1b45a298e7d8bf
sairoutine <sairoutine@gmail.com>
2017-03-22 02:00:50 +0900

    add: auto_fd and socket_arguments struct

--- src/app.cpp
+++ src/app.cpp
@@ -1,5 +1,46 @@
+#include <unistd.h>
 #include <string>
+#include "yakumo.h"
 namespace yakumo {
+	struct socket_arguments {
+		int family;
+		int msg_timeout;
+		int nodelay;
+		int protocol;
+		int socktype;
+		int timeout;
+		bool nonblocking;
+		bool reuseaddr;
+		bool use_epoll;
+	};
+
+
+	struct auto_fd {
+		auto_fd() : fd(-1) { }
+		~auto_fd() {
+			reset();
+		}
+		int get() const { return fd; }
+		int close() {
+			int r = 0;
+			if (fd < 0) {
+				return r;
+			}
+
+			r = ::close(fd);
+			fd = -1;
+			return r;
+		}
+		void reset() {
+			if (fd >= 0) {
+				close();
+			}
+		}
+
+		private:
+			int fd;
+	};
+
 	struct app {
 		app();
 		~app();
